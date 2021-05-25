f28d655f84eaf26bfcd4c610b1dc8235d7f231ed
jinnaichen <jinnaichen@huya.com>
2021-05-25 11:04:45 +0800

    Initial Commit

---
+++ Photos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Photos
+//
+//  Created by jinnaichen on 2021/5/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
